4dbd2e2d0eb2b266afaedae59b7e955f778eb6fa
Tim Trippel <ttrippel@google.com>
2020-05-28 21:29:02 +0000

    Updated common make file to decouple building software model of the hardware and the generating a simulatable exe

--- circuits/common.mk
+++ circuits/common.mk
@@ -8,19 +8,19 @@ else
 	VFLAGS := --top-module $(DUT) --Mdir $(OBJ_DIR) --trace $(COVERAGE) --cc
 endif
 
-$(OBJ_DIR)/V$(DUT): V$(DUT).mk
-	make -C $(OBJ_DIR) -f $^
-
 V$(DUT).mk: $(SRCS) $(TB)
 	verilator $(CFLAGS) -LDFLAGS $(LIBS) $(VFLAGS) $(SRCS) --exe $(TB)
 
-.PHONY: all clean coverage seed sim
+.PHONY: clean coverage exe seed sim
 
 coverage:
 	verilator_coverage --annotate logs/annotated logs/coverage.dat
 
 sim: $(OBJ_DIR)/V$(DUT)
 	./$^ $(INPUT) $(DUT).vcd
+
+exe: V$(DUT).mk
+	make -C $(OBJ_DIR) -f $^
 
 clean:
 	rm -rf $(OBJ_DIR)
