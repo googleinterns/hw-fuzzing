260d042b4faaa20782792821777aa6d091d49ab0
Tim Trippel <ttrippel@google.com>
2020-05-29 20:49:22 +0000

    adjusted makefile to make building circuit models compatible with older versions of gcc

--- circuits/common.mk
+++ circuits/common.mk
@@ -1,15 +1,13 @@
-#CC      := verilator
-CFLAGS  := -Wno-fatal -O2
 OBJ_DIR := model
 
 ifndef COVERAGE
-	VFLAGS := --top-module $(DUT) --Mdir $(OBJ_DIR) --trace --cc
+	VFLAGS := -Wno-fatal -O2 --top-module $(DUT) --Mdir $(OBJ_DIR) --trace --cc
 else
-	VFLAGS := --top-module $(DUT) --Mdir $(OBJ_DIR) --trace $(COVERAGE) --cc
+	VFLAGS := -Wno-fatal -O2 --top-module $(DUT) --Mdir $(OBJ_DIR) --trace $(COVERAGE) --cc
 endif
 
 V$(DUT).mk: $(SRCS) $(TB)
-	verilator $(CFLAGS) -LDFLAGS $(LIBS) $(VFLAGS) $(SRCS) --exe $(TB)
+	verilator $(VFLAGS) -CFLAGS -std=c++11 -LDFLAGS $(LIBS) $(VFLAGS) $(SRCS) --exe $(TB)
 
 .PHONY: clean coverage exe seed sim
 
