2a7afe37ff4031a8707954b82af2facfafc51618
timothytrippel <trippel@umich.edu>
2020-11-11 04:21:07 +0000

    Fixed bugs in AES smoke test script and AES seeds.
    Signed-off-by: timothytrippel <trippel@umich.edu>

--- tests/aes_hwf_isa_smoke_test.py
+++ tests/aes_hwf_isa_smoke_test.py
@@ -13,12 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import errno
 import glob
 import itertools
 import os
 import shutil
 import sys
+import threading
+import time
 
 import hjson
 from hwfp.fuzz import fuzz
@@ -36,6 +39,7 @@ DECRYPT_LOG = "decrypt.results.txt"
 
 TERMINAL_ROWS, TERMINAL_COLS = os.popen('stty size', 'r').read().split()
 LINE_SEP = "=" * int(TERMINAL_COLS)
+TEST_IS_EXECUTING = False
 
 
 class AESTestParams:
@@ -80,6 +84,15 @@ class AESTestPair:
         "encrypt", "x", 1)
 
 
+def _waiting_animation():
+  for c in itertools.cycle(['|', '/', '-', '\\']):
+    if not TEST_IS_EXECUTING:
+      break
+    print("\rexecuting " + c, end="")
+    sys.stdout.flush()
+    time.sleep(0.5)
+
+
 def _get_next_test_param(yaml_seed, yaml_seed_file, expected_key):
   line = yaml_seed_file.readline()
   line_list = line.lstrip("# ").rstrip().split("=")
@@ -97,27 +110,33 @@ def _extract_crypt_io_log_lines(yaml_seeds_dir, yaml_seed):
   data_block_size = 0
   data_in_line_starts = []
   data_out_line_starts = []
-  with open(os.path.join(yaml_seeds_dir, yaml_seed), "r+") as ys:
+  with open(os.path.join(yaml_seeds_dir, yaml_seed), "r") as ys:
     for line in ys:
       line = line.rstrip().lstrip("# ")
       if line == "TEST VARIABLES":
         ys.readline()
         # subtract three to account for other text in log
-        num_crypts = _get_next_test_param(yaml_seed, ys, "NUM_CRYPTS") - 3
+        num_crypts = _get_next_test_param(yaml_seed, ys, "NUM_CRYPTS")
         data_block_size = _get_next_test_param(yaml_seed, ys,
                                                "DATA_BLOCK_SIZE")
         for crypt_num in range(1, num_crypts + 1):
           data_in_line_starts.append(
-              _get_next_test_param(yaml_seed, ys,
-                                   "DATA_IN_LINE_START_" + str(crypt_num)))
+              _get_next_test_param(yaml_seed, ys, "DATA_IN_LINE_START_" +
+                                   str(crypt_num)) - 3)
           data_out_line_starts.append(
-              _get_next_test_param(yaml_seed, ys,
-                                   "DATA_OUT_LINE_START_" + str(crypt_num)))
+              _get_next_test_param(yaml_seed, ys, "DATA_OUT_LINE_START_" +
+                                   str(crypt_num)) - 3)
+  if (num_crypts == 0 or data_block_size == 0 or
+      len(data_in_line_starts) == 0 or len(data_out_line_starts) == 0):
+    print(red("ERROR: could not retrieve test params from YAML seed (%s)." %
+              yaml_seed),
+          file=sys.stderr)
+    sys.exit(1)
   return AESTestParams(num_crypts, data_block_size, data_in_line_starts,
                        data_out_line_starts)
 
 
-def _extract_test_pairs(yaml_seeds_dir):
+def _extract_test_pairs(yaml_seeds_dir, testcase=None):
   test_pairs = []
   encrypt_seeds = glob.glob(os.path.join(yaml_seeds_dir, "*encrypt*.yml"))
   decrypt_seeds = glob.glob(os.path.join(yaml_seeds_dir, "*decrypt*.yml"))
@@ -128,7 +147,14 @@ def _extract_test_pairs(yaml_seeds_dir):
   if len(encrypt_seeds) != len(decrypt_seeds):
     print(red("ERROR: non-symmetric number of encrypt/decrypt seeds."),
           file=sys.stderr)
+    sys.exit(1)
   for i in range(len(encrypt_seeds)):
+    if testcase is not None:
+      prefix = testcase.split("x")[0]
+      suffix = "".join([testcase.split("x")[-1], ".yml"])
+      if (not encrypt_seeds[i].startswith(prefix) or
+          not encrypt_seeds[i].endswith(suffix)):
+        continue
     # Extract test details from comments in yaml file
     encrypt_test_params = _extract_crypt_io_log_lines(yaml_seeds_dir,
                                                       encrypt_seeds[i])
@@ -217,19 +243,26 @@ def _check_simulation_results(encrypt_results, decrypt_results, test_pair):
         len(encrypt_in_data) != len(decrypt_out_data)):
       error = True
       error_msg = "data LENGTH MISMATCH for encrypt/decrypt data blocks."
-      break
+      # break
     # Check inputs/outputs of encrypt/decrypt match
     if not error:
       for i in range(len(encrypt_out_data)):
+        print("   Encrypt Out =  0x{:0>8X}; Decrypt In = 0x{:0>8X}".format(
+            encrypt_out_data[i], decrypt_in_data[i]))
         if encrypt_out_data[i] != decrypt_in_data[i]:
           error_msg = "encryption OUTPUT does not match decryption INPUT."
           error = True
           break
+    print("   --------------------------------------------")
     if not error:
       for i in range(len(encrypt_in_data)):
+        print("   Encrypt In =  %d; Decrypt Out = %d" %
+        print("   Encrypt In =  0x{:0>8X}; Decrypt Out = 0x{:0>8X}".format(
+              encrypt_in_data[i], decrypt_out_data[i]))
         if encrypt_in_data[i] != decrypt_out_data[i]:
           error_msg = "encryption INPUT does not match decryption OUTPUT."
           break
+    print("   --------------------------------------------")
     # If an error occured during any test, terminate
     if error:
       break
@@ -237,10 +270,19 @@ def _check_simulation_results(encrypt_results, decrypt_results, test_pair):
   if not error:
     print(green("PASS"))
   else:
-    print("".join(red("ERROR"), ":", error_msg))
+    print("".join([red("ERROR"), ":", error_msg]))
 
 
-def _main():
+def _main(argv):
+  # Part command line arguments
+  parser = argparse.ArgumentParser(description="AES Smoke Test")
+  parser.add_argument(
+      "-t",
+      "--testcase",
+      default=None,
+      help="Specific testcase to run, otherwise all tests are run.")
+  args = parser.parse_args(argv)
+
   # Print initial message with # of total tests
   isas = list(itertools.product(OPCODE_TYPES, INSTR_TYPES, TERMINATE_TYPES))
   num_tests = len(isas)
@@ -250,7 +292,7 @@ def _main():
   # Get list of encryption/decryption test pairs
   yaml_descripts_dir = os.path.join(os.getenv("HW_FUZZING"), "hw", "aes",
                                     "seed_descriptions")
-  test_pairs = _extract_test_pairs(yaml_descripts_dir)
+  test_pairs = _extract_test_pairs(yaml_descripts_dir, args.testcase)
 
   test_suite_num = 1
   for opcode_type, instr_type, terminate_type in isas:
@@ -271,11 +313,14 @@ def _main():
         print("(%s)\t- " % tp.test_id, end="")
         sys.stdout.flush()
 
-        # Run encryption simulation
+        # Run simulations in separate thread to show animated waiting spinner
+        # TEST_IS_EXECUTING = True
+        # t = threading.Thread(target=_waiting_animation)
+        # t.daemon = True
+        # t.start()
         _run_simulation(config_dict, ENCRYPT_LOG, tp.encrypt)
-
-        # Run decryption simulation
         _run_simulation(config_dict, DECRYPT_LOG, tp.decrypt)
+        # TEST_IS_EXECUTING = False
 
         # Check simulation results
         _check_simulation_results(ENCRYPT_LOG, DECRYPT_LOG, tp)
@@ -292,4 +337,4 @@ def _main():
 
 
 if __name__ == "__main__":
-  _main()
+  _main(sys.argv[1:])
