e9e67132d04c7b552f5b9b9f6c3da3ba82be63e0
timothytrippel <trippel@umich.edu>
2020-11-12 14:04:30 +0000

    Refactored HWF ISA experiment script to run on several different OT cores.
    Signed-off-by: timothytrippel <trippel@umich.edu>

--- experiment_scripts/exp005_hwf_isa_otcores.py
+++ experiment_scripts/exp005_hwf_isa_otcores.py
@@ -14,8 +14,10 @@
 # limitations under the License.
 
 import copy
+import glob
 import itertools
 import os
+import shutil
 
 import hjson
 from hwfp.fuzz import fuzz
@@ -23,22 +25,30 @@ from hwfutils.string_color import color_str_green as green
 
 from ot_config_dict import CONFIG_DICT
 
-TOPLEVEL = "aes"
+# ------------------------------------------------------------------------------
+# Experiment Parameters
+# ------------------------------------------------------------------------------
 EXPERIMENT_BASE_NAME = "exp008-cpp-afl-%s-%s-%s-%s-%d"
+DURATION_MINS = 60
+# DURATION_MINS = 1440  # 24 hours
+# TOPLEVELS = ["aes", "hmac", "kmac", "rv_timer"]
+TOPLEVELS = ["aes", "hmac"]
 OPCODE_TYPES = ["constant", "mapped"]
 INSTR_TYPES = ["variable", "fixed"]
 TERMINATE_TYPES = ["invalidop", "never"]
 # RUNS = range(0, 20)
-RUNS = range(0, 3)
+RUNS = range(0, 2)
+# ------------------------------------------------------------------------------
 
 TERMINAL_ROWS, TERMINAL_COLS = os.popen('stty size', 'r').read().split()
 LINE_SEP = "=" * int(TERMINAL_COLS)
 
 
 def _main():
-  isas = list(itertools.product(OPCODE_TYPES, INSTR_TYPES, TERMINATE_TYPES))
+  isas = list(
+      itertools.product(TOPLEVELS, OPCODE_TYPES, INSTR_TYPES, TERMINATE_TYPES))
   print(LINE_SEP)
-  print(green("LAUNCHING %d EXPERIMENTS ..." % (len(isas) * len(RUNS))))
+  print(green("Launching %d experiments ..." % (len(isas) * len(RUNS))))
   print(LINE_SEP)
 
   # create a temp dir to store config files
@@ -51,19 +61,20 @@ def _main():
     os.mkdir(tmp_dir)
 
     # create config files on the fly and launch experiments
-    for opcode_type, instr_type, terminate_type in isas:
+    for toplevel, opcode_type, instr_type, terminate_type in isas:
       for run in RUNS:
         # craft config dictionary
         cdict = copy.deepcopy(CONFIG_DICT)
 
         # Set experiment name
         experiment_name = EXPERIMENT_BASE_NAME % (
-            TOPLEVEL, opcode_type, instr_type, terminate_type, run)
+            toplevel, opcode_type, instr_type, terminate_type, run)
         print(experiment_name)
         cdict["experiment_name"] = experiment_name
-        cdict["toplevel"] = TOPLEVEL
+        cdict["toplevel"] = toplevel
 
         # Set configurations
+        cdict["fuzzer_params"]["duration_mins"] = DURATION_MINS
         cdict["model_params"]["opcode_type"] = opcode_type
         cdict["model_params"]["instr_type"] = instr_type
         if terminate_type == "invalidop":
@@ -84,7 +95,8 @@ def _main():
         os.remove(hjson_file_path)
 
   finally:
-    os.rmdir(tmp_dir)
+    for directory in glob.glob("tmp*"):
+      shutil.rmtree(directory)
 
   print(LINE_SEP)
   print(green("DONE!"))
