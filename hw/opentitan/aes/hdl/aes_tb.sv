aca66f39dcffe44dde152f522c8449975424e6b8
timothytrippel <trippel@umich.edu>
2021-05-30 13:35:02 -0400

    Altered testbench to detect a different FSM bug and updated seed file to only initialize the core for encryption.
    Signed-off-by: timothytrippel <trippel@umich.edu>

--- hw/opentitan/aes/hdl/aes_tb.sv
+++ hw/opentitan/aes/hdl/aes_tb.sv
@@ -62,7 +62,7 @@ module aes_tb
 `ifdef INJECTED_BUG_CHECK
   logic [2:0] ctrl_cs;
   assign ctrl_cs = dut.u_aes_core.u_aes_cipher_core.u_aes_cipher_control.aes_cipher_ctrl_cs;
-  `HWF_ASSERT(FSMBug, (ctrl_cs == 4) |=> (ctrl_cs == 5))
+  `HWF_ASSERT(FSMBug, (ctrl_cs == 3) |=> (ctrl_cs == 0))
 `endif
 
 endmodule
